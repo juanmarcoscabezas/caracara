5da353fc112464e383369ba6a22cfab27baee4bf
noa21 <juanmarcoscabezas@gmail.com>
2019-06-09 12:04:26 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const hbs = require('express-handlebars');
+const path = require('path');
+
+// CONFIGURACION HANDLEBARS
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('partials', path.join(__dirname, 'partials'));
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('partials'), 'partials'),
+}));
+
+// RUTAS
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+io.on('connection', function(socket) {
+    console.log('Se ha conectado alguien');
+});
+
+//SOCKET
+http.listen(3000, function() {
+    console.log('Escuchando en el puerto 3000');
+});
